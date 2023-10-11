53d0a4e990456948e08a52229a599dd0908078c3
cantwork <t.co@qq.com>
2023-10-11 19:26:35 +0800

    Update wsj.js

--- wsj.js
+++ wsj.js
@@ -3,7 +3,7 @@
 // @version         1.0
 // @downloadURL     https://raw.githubusercontent.com/cantwork/UserScripts/main/wsj.js
 // @updateURL       https://raw.githubusercontent.com/cantwork/UserScripts/main/wsj.js
-// @license         MIT
+// @license         MIT; https://gitlab.com/magnolia1234/bypass-paywalls-clean-filters/-/blob/main/LICENSE
 // @match           *://*.com/*
 // @match           *://*.wsj.com/*
 // ==/UserScript==
