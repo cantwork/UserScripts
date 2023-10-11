08067ad521bdd3d4c81875c87e6932cad1bb6345
cantwork <t.co@qq.com>
2023-10-11 19:29:52 +0800

    Update wsj.js

--- wsj.js
+++ wsj.js
@@ -5,7 +5,33 @@
 // @updateURL       https://raw.githubusercontent.com/cantwork/UserScripts/main/wsj.js
 // @license         MIT; https://gitlab.com/magnolia1234/bypass-paywalls-clean-filters/-/blob/main/LICENSE
 // @match           *://*.com/*
-// @match           *://*.wsj.com/*
+// @match           *://*.co.uk/*
+// @match           *://*.com.au/*
+// @match           *://*.io/*
+// @match           *://*.net/*
+// @match           *://*.net.au/*
+// @match           *://*.org/*
+// @match           *://*.pub/*
+// @match           *://*.businesspost.ie/*
+// @match           *://*.europower.no/*
+// @match           *://*.fiskeribladet.no/*
+// @match           *://*.hindutamil.in/*
+// @match           *://*.independent.ie/*
+// @match           *://*.indiatoday.in/*
+// @match           *://*.intrafish.no/*
+// @match           *://*.ipolitics.ca/*
+// @match           *://*.japantimes.co.jp/*
+// @match           *://*.jgnt.co/*
+// @match           *://*.livelaw.in/*
+// @match           *://*.nautil.us/*
+// @match           *://*.niagarafallsreview.ca/*
+// @match           *://*.nzherald.co.nz/*
+// @match           *://*.puck.news/*
+// @match           *://*.sloanreview.mit.edu/*
+// @match           *://*.stcatharinesstandard.ca/*
+// @match           *://*.uxdesign.cc/*
+// @match           *://*.wellandtribune.ca/*
+// @exclude         *://*.google.com/*
 // ==/UserScript==
 
 (function() {
